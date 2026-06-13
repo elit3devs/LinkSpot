f964ad29aa00824bd5ec36a4de64ec4454942435
Elit3 <bob691388@gmail.com>
2026-06-12 21:27:31 -0400

    Re-add ultraviolet integration

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import http from "node:http";
 import path from "node:path";
 import { createBareServer } from "@tomphttp/bare-server-node";
+import { Ultraviolet } from "@titaniumnetwork-dev/ultraviolet";
 import chalk from "chalk";
 import express from "express";
 import basicAuth from "express-basic-auth";
@@ -15,6 +16,8 @@ const bareServer = createBareServer("/edu/", {
   logLevel: "info",
   localAddress: "0.0.0.0"
 });
+
+const uv = new Ultraviolet("https://raw.githubusercontent.com/titaniumnetwork-dev/Ultraviolet-Static/main/uv/uv.config.js");
 
 const PORT = process.env.PORT || 80;
 
@@ -38,6 +41,16 @@ app.use(
 );
 
 app.get('/scramjet/*', (req, res) => {
+  const encodedUrl = req.params[0];
+  try {
+    const url = Buffer.from(encodedUrl, 'base64').toString('utf-8');
+    res.redirect('/edu/' + url);
+  } catch (e) {
+    res.status(400).send('Invalid URL');
+  }
+});
+
+app.get('/uv/*', (req, res) => {
   const encodedUrl = req.params[0];
   try {
     const url = Buffer.from(encodedUrl, 'base64').toString('utf-8');
